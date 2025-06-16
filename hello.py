8f2270355121b426ed74c1bc67360a5252f3b317
Pratikgurung647558 <gurungpratik678@gmail.com>
2025-06-16 20:40:35 +0545

    completed

--- hello.py
+++ hello.py
@@ -1,1 +1,36 @@
 print("first python project")
+import random
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+
+comp=random.choice([-1,0,1])
+player=(input("enter "))
+dic={"s":1,"w":-1,"g":0}
+reversedict={1:"snake",-1:"water",0:"gun"}
+
+you=dic[player]
+print("comp choose",reversedict[comp])
+
+
+if (comp==you):
+    print('draw')
+else:
+    if (comp==-1 and you==1):
+        print("you win")
+    elif (comp==-1 and you==0):
+        print("you lose")
+    elif (comp==1 and you==-1):
+        print("you lose")
+    elif (comp==1 and you==0):
+        print("you win")
+    elif (comp==0 and you==-1):
+        print("you lose")
+    elif (comp==0 and you==1):
+        print("you win")
+    else:
+        print("error")
+    
+
